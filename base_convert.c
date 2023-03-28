6c010c40fc8d1aa80b79efdb32f3962e95846a18
Eman288 <emantamam107@gmail.com>
2023-03-28 14:26:31 -0700

    Task00

--- base_convert.c
+++ base_convert.c
@@ -17,8 +17,6 @@ int print_binary(va_list list)
 	num = va_arg(list, unsigned int);
 	if (num == 0)
 		return (_write_char('0'));
-	if (num < 0)
-		return (-1);
 	len = base_len(num, 2);
 	string = malloc(sizeof(char) * len + 1);
 	if (string == NULL)
