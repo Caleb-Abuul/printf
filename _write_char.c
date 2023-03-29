58e97a66feb98b6e180522249a85477cde70abdc
Caleb-Abuul <abuulcaleb@gmail.com>
2023-03-29 09:16:09 -0700

    task 4

--- _write_char.c
+++ _write_char.c
@@ -9,3 +9,24 @@ int _write_char(char c)
 {
 	return (write(1, &c, 1));
 }
+/**
+ * buffer - saves character
+ * @c: the character to be saved in buffer
+ * Return: 1.
+ */
+int buffer(char c)
+{
+	static char buffering[1024];
+	static int i;
+
+	if (c == -1 || i == 1024)
+	{
+		write(1, buffering, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buffering[i++] = c;
+	}
+	return (1);
+}
